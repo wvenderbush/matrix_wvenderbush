0a53b2e28f981129a4367962ef50e2c1b0783d09
Winston Venderbush <winstonvenderbush@gmail.com>
2017-02-27 11:30:34 -0500

    added test cases

--- main.py
+++ main.py
@@ -1,11 +1,44 @@
 from display import *
 from draw import *
+import random
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
 matrix = new_matrix()
 
 
+#print_matrix(matrix)
+
+#Identity Matrix
+print "\nID Matrix\n"
+idmatrix = new_matrix()
+ident(idmatrix)
+print_matrix(idmatrix)
+
+#Scalar Multiplication
+print "\nScalar Multiplication by 2\n"
+print_matrix(idmatrix)
+print "(x2) becomes..."
+scalar_mult( idmatrix, 2 )
+print_matrix(idmatrix)
+
+#Matrix Multiplication
+print "\nMatrix Multiplication\n"
+w, h = 4, 4;
+m1 = [[random.randrange(0, 10) for x in range(w)] for y in range(h)] 
+m2 = [[random.randrange(0, 10) for x in range(w)] for y in range(h)] 
+print "M1"
+print_matrix(m1)
+print "\nM2"
+print_matrix(m2)
+print "\nM1 x M2 Result"
+print_matrix(matrix_mult(m1, m2))
+
+
+
+
+
+#Generating Image with Matrix
 
 add_edge( matrix, 250, 0, 0, 500, 250, 0)
 add_edge( matrix, 250, 500, 0, 500, 250, 0)
@@ -23,10 +56,6 @@ add_edge( matrix, 125, 500, 0, 375, 0, 0)
 
 add_edge( matrix, 0, 375, 0, 500, 125, 0)
 add_edge( matrix, 0, 125, 0, 500, 375, 0)
-
-print_matrix(matrix)
-
-
 
 draw_lines( matrix, screen, color )
 display(screen)
